8402dc0ee5c76323b8cc734d46efdb29268ef110
Emmanuel Nkrumah-Sarpong <nkrumahthis@gmail.com>
2024-07-15 07:36:55 +0000

    implement select all function

--- tdl.scala
+++ tdl.scala
@@ -1,11 +1,22 @@
 import scala.util.{Try, Success, Failure}
 import java.io.{BufferedWriter, FileWriter, File}
+import scala.io.{Source, BufferedSource}
 
 class Database(val dbFilename: String):
     def insert(record: String): Try[Unit] =
         writeToFile(List(record), true)
-        
-    def selectAll(): Try[Seq[String]] = ???
+
+    def selectAll(): Try[Seq[String]] = 
+        var bufferedSource: BufferedSource = null
+        try 
+            bufferedSource = Source.fromFile(dbFilename)
+            val lines = for line <- bufferedSource.getLines yield line
+            Success(lines.toList)
+        catch
+            case t: Throwable => Failure(t)
+        finally
+            if bufferedSource != null then bufferedSource.close
+            
     def delete(indexToDelete: Int): Try[Int] = ???
 
     /**
